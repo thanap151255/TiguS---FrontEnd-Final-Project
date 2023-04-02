f0dfb2e00a4521ae804e3353e2d1d76a4527c9c3
thanapdev <thanap151255@gmail.com>
2023-04-03 01:16:11 +0700

    back space commit

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
- 
+
 db.connect();
 
 app.use(
